019743788c8802fbeb8bd20c87b1e0399d765e70
Tiago Melo Juca <contact@tiagomelojuca.com>
2025-05-24 15:51:13 -0300

    implementação abb "vanilla"

--- src/persistencia/abb.h
+++ src/persistencia/abb.h
@@ -23,6 +23,15 @@ namespace persistencia
 class abb
 {
 public:
+    struct noh
+    {
+        int chave = 0;
+
+        noh* pai = nullptr;
+        noh* esq = nullptr;
+        noh* dir = nullptr;
+    };
+
     bool inclui(int chave)
     {
         return false;
@@ -44,6 +53,135 @@ public:
     }
 
 private:
+    // Referencias:
+    // https://www.youtube.com/watch?v=f7sIuYI5M2Y
+    // https://www.youtube.com/watch?v=QA2wFn9nQU4
+
+    const noh* busca(const noh* x, int chave) const
+    {
+        while (x != nullptr && x->chave != chave)
+        {
+            x = chave < x->chave ? x->esq : x->dir;
+        }
+
+        return x;
+    }
+
+    noh* min(noh* x) const
+    {
+        while (x->esq != nullptr)
+        {
+            x = x->esq;
+        }
+
+        return x;
+    }
+
+    noh* max(noh* x) const
+    {
+        while (x->dir != nullptr)
+        {
+            x = x->dir;
+        }
+
+        return x;
+    }
+
+    noh* sucessor(const noh* x) const
+    {
+        if (x->dir != nullptr)
+        {
+            return min(x->dir);
+        }
+
+        noh* y = x->pai;
+        while (y != nullptr && x == y->dir)
+        {
+            x = y;
+            y = x->pai;
+        }
+
+        return y;
+    }
+
+    void inclui(noh* z)
+    {
+        noh* y = nullptr;
+        noh* x = raiz;
+
+        while (x != nullptr)
+        {
+            y = x;
+            x = z->chave < x->chave ? x->esq : x->dir;
+        }
+
+        z->pai = y;
+        if (y == nullptr)
+        {
+            raiz = z;
+        }
+        else if (z->chave < y->chave) {
+            y->esq = z;
+        }
+        else {
+            y->dir = z;
+        }
+    }
+
+    void remove(const noh* z)
+    {
+        if (z->esq == nullptr)
+        {
+            transplanta(z, z->dir);
+        }
+        else if (z->dir == nullptr)
+        {
+            transplanta(z, z->esq);
+        }
+        else
+        {
+            noh* y = sucessor(z);
+            transplanta(y, y->dir);
+
+            y->esq = z->esq;
+            z->esq->pai = y;
+            y->dir = z->dir;
+
+            if (z->dir != nullptr)
+            {
+                z->dir->pai = y;
+            }
+            transplanta(z, y);
+        }
+    }
+
+    void transplanta(const noh* u, noh* v)
+    {
+        if (u == nullptr)
+        {
+            return;
+        }
+
+        if (u->pai == nullptr)
+        {
+            raiz = v;
+        }
+        else if (u == u->pai->esq)
+        {
+            u->pai->esq = v;
+        }
+        else
+        {
+            u->pai->dir = v;
+        }
+
+        if (v != nullptr)
+        {
+            v->pai = u->pai;
+        }
+    }
+
+    noh* raiz = nullptr;
     int versao = 0;
 };
 
